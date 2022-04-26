b980f99cb1375e225ede730c28832c558f36823a
TheHuy <TheHuy@HUYPC>
2022-04-26 21:53:37 +0700

    Add project files.

---
+++ MACAddress/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.NetworkInformation;
+
+ string GetDeviceInfo()
+{
+    string mac = string.Empty;
+    string ip = string.Empty;
+
+    foreach (var netInterface in NetworkInterface.GetAllNetworkInterfaces())
+    {
+        if (netInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ||
+            netInterface.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+        {
+            var address = netInterface.GetPhysicalAddress();
+            mac = BitConverter.ToString(address.GetAddressBytes());
+
+            IPAddress[] addresses = Dns.GetHostAddresses(Dns.GetHostName());
+            if (addresses != null && addresses[0] != null)
+            {
+                ip = addresses[0].ToString();
+                break;
+            }
+        }
+    }
+
+    return mac;
+}
+Console.WriteLine($"MACAddreess :{GetDeviceInfo()}" );
